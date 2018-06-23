b77c9c0703f18b85416f6c2c8354cc756d4e64e4
Oleksii Kondratov <alexei.kondratov@gmail.com>
2018-06-23 18:04:36 +0300

    Made AnimalEventJsonConverter non case sensitive.

---
+++ AnimalFarm.AnimalService.Tests/AnimalEventJsonConverterTests.cs
@@ -0,0 +1,44 @@
+﻿using AnimalFarm.Model.Events;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace AnimalFarm.AnimalService.Tests
+{
+    public class AnimalEventJsonConverterTests
+    {
+        public static IEnumerable<object[]> GetDataForAnimalEventJsonConverter_deserializes_an_animal_event
+            => new object[][] {
+                new object []
+                {
+                    JsonConvert.SerializeObject(new CreateAnimalEvent { EventId = "SomeEvent" }).Replace("EventType", "eventType"),
+                    typeof(CreateAnimalEvent),
+                    "SomeEvent"
+                },
+                new object []
+                {
+                    JsonConvert.SerializeObject(new AnimalActionEvent { EventId = "AnotherEvent" }).Replace("Interact", "interact"),
+                    typeof(AnimalActionEvent),
+                    "AnotherEvent"
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(GetDataForAnimalEventJsonConverter_deserializes_an_animal_event))]
+        public void AnimalEventJsonConverter_deserializes_an_animal_event(string json, Type expectedType, string expectedId)
+        {
+            // Arrange
+            var target = new AnimalEventJsonConverter();
+            var jsonReader = new JsonTextReader(new StringReader(json));
+
+            // Act
+            var result = target.ReadJson(jsonReader, typeof(AnimalEvent), null, new JsonSerializer()) as AnimalEvent;
+
+            // Assert
+            Assert.IsType(expectedType, result);
+            Assert.Equal(expectedId, result.EventId);
+        }
+    }
+}
--- AnimalFarm.AnimalService/AnimalEventJsonConverter.cs
+++ AnimalFarm.AnimalService/AnimalEventJsonConverter.cs
@@ -2,34 +2,38 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Linq;
 
 namespace AnimalFarm.AnimalService
 {
-    internal class AnimalEventJsonConverter : JsonConverter
+    public class AnimalEventJsonConverter : JsonConverter
     {
         public override bool CanConvert(Type objectType)
         {
             return typeof(AnimalEvent).IsAssignableFrom(objectType);
         }
 
+        private AnimalEvent InstantiateByTypeName(string eventTypeName)
+        {
+            switch (eventTypeName)
+            {
+                case "interact":
+                    return new AnimalActionEvent();
+                case "create":
+                    return new CreateAnimalEvent();
+                default:
+                    throw new NotSupportedException(eventTypeName);
+            }
+        }
+        
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             JObject jObj = JObject.Load(reader);
-            string typeName = (string)jObj[nameof(AnimalEvent.EventType)];
+            bool isEventTypeProperty(JProperty p) 
+                => String.Equals(p.Name, nameof(AnimalEvent.EventType), StringComparison.InvariantCultureIgnoreCase);
 
-            AnimalEvent result;
-            switch (typeName)
-            {
-                case "Interact":
-                    result = new AnimalActionEvent();
-                    break;
-                case "Create":
-                    result = new CreateAnimalEvent();
-                    break;
-                default:
-                    throw new NotImplementedException();
-            }
-
+            string typeName = ((string)jObj.Children<JProperty>().First(isEventTypeProperty).Value).ToLower();
+            AnimalEvent result = InstantiateByTypeName(typeName);
             serializer.Populate(jObj.CreateReader(), result);
             return result;
         }
